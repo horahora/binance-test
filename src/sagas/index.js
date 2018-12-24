6aae22caa968ebe395b4572ad4e864e34035b3fe
qiujunbin <junbin.qiu@fir.ai>
2018-12-24 20:30:16 +0800

    .

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,24 +1,43 @@
-import { put, call, fork, all, takeEvery } from 'redux-saga/effects'
+import { put, call, fork, all, take, takeEvery, select } from 'redux-saga/effects'
 // import { pingRequested } from '../actions'
 import { fetchPing } from '../services/api'
-import { PING_REQUESTED, PING_SUCCEEDED } from '../constants/ActionTypes'
+import { PING_REQUESTED, PING_SUCCEEDED, PING_FAILED } from '../constants/ActionTypes'
 
 function* pingTest() {
-  console.log(fetchPing);
-  const res = yield call(fetchPing)
-  yield put({type: PING_SUCCEEDED, res})
+  try {
+    console.log(fetchPing);
+    const data = yield call(fetchPing)
+    console.log('lll',data);
+    yield put({type: PING_SUCCEEDED, data})
+  } catch (error) {
+    yield put({type: PING_FAILED, error })
+  }
+
+}
+
+function* watchAndLog() {
+  while (true) {
+    const action = yield take('*')
+    const state = yield select()
+    console.log('action', action)
+    console.log('state after', state)
+  }
 }
 
 function* watchPingTest() {
-  yield takeEvery(PING_REQUESTED, pingTest)
+  while (true) {
+    yield take(PING_REQUESTED)
+    yield call(pingTest)
+  }
 }
 
 /******************************************************************************/
 /******************************* WATCHERS *************************************/
 /******************************************************************************/
 
-export default function* root() {
+export default function* rootSaga() {
   yield all([
-    fork(watchPingTest)
+    fork(watchPingTest),
+    fork(watchAndLog)
   ])
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,13 @@
 import axios from 'axios'
 
 
-export const fetchPing = axios.get('/api/v1/ping')
+function request(url, data) {
+  return axios.get('/api/v1/ping')
+}
+
+export function fetchPing() {
+  return axios.get('/api/v1/ping')
+    .then(res => {
+      console.log(res)
+    })
+}
